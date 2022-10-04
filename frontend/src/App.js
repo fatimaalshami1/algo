632794f38bfc913e841efaa03bf74c19532a1af4
prat <pratikkatti4@gmial.com>
2022-10-04 22:33:21 +0530

    top bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./app.css"
+import TopBar from "./topbar/TopBar";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <TopBar/>
+        <h1 className="text">ALGORITHM VISUALIZER</h1>
+        <h4 className="text1">Visualize Algoritm For Better Understanding</h4>
       </header>
     </div>
   );
