c5269ae8498961f2e438a6d4c9fbfb946d7c6a60
prat <pratikkatti4@gmial.com>
2022-10-05 17:26:50 +0530

    before routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import "./app.css"
+import Card from "./cards/Card";
 import TopBar from "./topbar/TopBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
+    // <Router>
       <header className="App-header">
        
         <TopBar/>
         <h1 className="text">ALGORITHM VISUALIZER</h1>
         <h4 className="text1">Visualize Algoritm For Better Understanding</h4>
+
+        <Card/>
       </header>
-    </div>
+    // </Router>
   );
 }
 
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+       <h1 className="text">ALGORITHM VISUALIZER</h1>
+        <h4 className="text1">Visualize Algoritm For Better Understanding</h4>
+
+        <Card/>
+    </div>
+  )
+}
